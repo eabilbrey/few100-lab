06a10f7be7d61a0e32c54e5e9c0fbbd4e62aa305
eabilbrey <59836327+eabilbrey@users.noreply.github.com>
2020-01-17 17:32:39 +0000

    IT again!

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+
+
+// 1. find the important things you need on the page.
+
+const tip10 = document.getElementById('tip10') as HTMLInputElement;
+const tip15 = document.getElementById('tip15') as HTMLInputElement;
+const tip20 = document.getElementById('tip20') as HTMLInputElement;
+const tipCustom = document.getElementById('tipCustom') as HTMLInputElement;
+const billTotal = document.getElementById('bill') as HTMLInputElement;
+const tipChoice = document.getElementById('tipChoice') as HTMLSpanElement;
+const billAmount = document.getElementById('billAmount') as HTMLSpanElement;
+const tipPercentageChosen = document.getElementById('tipPercentageChosen') as HTMLSpanElement;
+const tipAmount = document.getElementById('tipAmount') as HTMLSpanElement;
+const totalToBePaid = document.getElementById('totalToBePaid') as HTMLSpanElement;
+
+const splitYes = document.getElementById('yes') as HTMLInputElement;
+const splitNo = document.getElementById('no') as HTMLInputElement;
+
+const billSplit = document.getElementById('billSplit') as HTMLInputElement;
+const splitTotalToBePaid = document.getElementById('splitTotalToBePaid') as HTMLInputElement;
+const totalPerParty = document.getElementById('totalPerParty') as HTMLInputElement;
 
